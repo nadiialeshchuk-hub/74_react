f7a8c638f9263fbd9d7be8b42961b6193a0cef34
Nadiia Leshchuk <nadiialeshchuk@gmail.com>
2026-01-06 13:03:51 +0100

    Add project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState} from 'react';
+
+import './App.css';
+import Button  from './components/Button/Button';
+
+function App() {
+const [count, setCount] = useState(0);
+
+const onMinus = () => setCount((prev)=> prev - 1);
+
+  return (
+    <div className='app'>
+     React Introduction
+     {/* вызов компонента */}
+     <Button name='plus' onButtonClick={()=>setCount((prev)=> prev + 1)} />
+     <Button name='minus'onButtonClick={onMinus}/>
+     <div>{count}</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function Button ({name,onButtonClick}){ 
+   return (
+   <button className='button' onClick={onButtonClick}>
+    {name}
+    </button>
+   );
+}
+
+export default Button;
